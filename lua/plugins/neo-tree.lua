9a127cea88c176cbbe57f0c804bdac707e9c6108
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-10-06 11:01:54 +0545

    refactor: neo-tree.lua to show hidden file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,5 +5,12 @@ return {
       position = "right", -- Move the file tree to the right
       width = 50, -- You can adjust the width if needed
     },
+    filesystem = {
+      filtered_items = {
+        visible = true, -- Show hidden files
+        hide_dotfiles = false, -- Do not hide dotfiles (files starting with a .)
+        hide_gitignored = false, -- Show git ignored files as well (optional)
+      },
+    },
   },
 }
