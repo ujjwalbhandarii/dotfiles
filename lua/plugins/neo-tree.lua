9da861f5619da8c2b686822291f07b759b598459
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-10-06 10:56:00 +0545

    feat: neo-tree.lua | right side config

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right", -- Move the file tree to the right
+      width = 50, -- You can adjust the width if needed
+    },
+  },
+}
