aa67ebed357c34a9fc8f8b19988a3f5cbf0d28da
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-10-07 16:13:40 +0545

    added-packages: fugitive, tailwind, linting

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,12 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { "tpope/vim-fugitive" }, -- Add Fugitive for Git integration
     -- import/override with your plugins
     { import = "plugins" },
   },
