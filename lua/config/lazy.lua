6238b4de391965363eef71c069b675a0656d2798
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-10-17 08:46:20 +0545

    feat: add new language support packages in LazyVim
    - prisma
    - tailwind snippets
    - tabline ai code helper
    - pretty ts errors
    - path intellisense
    - git history

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,39 +20,58 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
       colorscheme = "vscode",
     } },
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+
+    -- Language support and snippets
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { "tpope/vim-fugitive" }, -- Add Fugitive for Git integration
+
+    -- AI code helper (Tabnine)
+    { "codota/tabnine-nvim", build = "./dl_binaries.sh" },
+
+    -- Prisma language support
+    { "pantharshit00/vim-prisma" },
+
+    -- Angular syntax support
+    { "jose-elias-alvarez/nvim-lsp-ts-utils", dependencies = { "neovim/nvim-lspconfig" } },
+    { "iamcco/markdown-preview.nvim", ft = { "markdown" }, build = "cd app && npm install" },
+
+    -- Git integration and Git history
+    { "tpope/vim-fugitive" }, -- Git integration
+    { "junegunn/gv.vim" }, -- Git history
+
+    -- Path intellisense
+    { "hrsh7th/cmp-path" }, -- Autocompletion for file paths
+
+    -- Pretty TypeScript errors
+    { "folke/trouble.nvim", dependencies = { "kyazdani42/nvim-web-devicons" } },
+
+    -- Tailwind snippets
+    { "EmmanuelMess/tailwindcss-snippets.nvim" }, -- Tailwind CSS snippets
+
     -- import/override with your plugins
     { import = "plugins" },
   },
+
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+
   install = { colorscheme = { "tokyonight", "habamax" } },
+
   checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+    enabled = true,
+    notify = false,
+  },
+
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
