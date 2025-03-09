45e70e6adb30616ebdfdf285499e8974e6aad3be
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-03-09 15:11:29 +0545

    feat: added harpoon and obsibian

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,18 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*",
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "notes",
+				path = "~/Documents/notes",
+			},
+		},
+		disable_frontmatter = true,
+	},
+}
