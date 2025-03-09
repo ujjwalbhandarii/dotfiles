9c9ab44d6557e9fba2beed77ec59793271932d53
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-03-09 15:51:54 +0545

    refactor: comment bindings

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,10 @@
+return {
+	"numToStr/Comment.nvim",
+	-- opts = {
+	-- 	toggler = {
+	-- 		line = "<C-/>", -- Use Ctrl + / for line comments
+	-- 		block = "<C-?>", -- Use Ctrl + ? for block comments
+	-- 	},
+	-- },
+	lazy = false,
+}
