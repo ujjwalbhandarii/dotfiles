eeccc66456606d400801b9b2e6d3db66ba5f585e
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-03-12 20:21:32 +0545

    updated lazy configs

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,14 +20,16 @@ require("lazy").setup({
 			import = "lazyvim.plugins",
 			opts = {
 				colorscheme = "tokyonight-night",
+				defaults = {
+					autocmds = true,
+					keymaps = true,
+					options = true,
+				},
 			},
 		},
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
-		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
-		{ import = "lazyvim.plugins.extras.formatting.prettier" },
-		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 		{ import = "plugins" },
 	},
 	defaults = {
