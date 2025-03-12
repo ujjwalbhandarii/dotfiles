7b10a4caf80b4069544b106bfa80d5317361d5f6
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-03-12 19:28:04 +0545

    updated ignore search files

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -29,11 +29,30 @@ vim.opt.tabstop = 2
 vim.opt.wrap = true
 vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.path:append({ "**" })
-vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = "a"
+
+vim.opt.wildignore:append({
+	"*/node_modules/*", -- Ignore Node.js modules
+	"*/venv/*", -- Ignore Python virtual environments (common name)
+	"*/env/*", -- Ignore Python virtual environments (another common name)
+	"*/.env/*", -- Ignore environment variable files
+	"*/__pycache__/*", -- Ignore Python bytecode cache directories
+	"*/.git/*", -- Ignore Git directories
+	"*/.pytest_cache/*", -- Ignore pytest cache directories
+	"*/.mypy_cache/*", -- Ignore mypy cache directories
+	"*/.venv/*", -- Ignore virtual environments (another common name)
+	"*/.tox/*", -- Ignore tox directories
+	"*/.idea/*", -- Ignore JetBrains IDE project files
+	"*/.vscode/*", -- Ignore Visual Studio Code project files
+	"*/storage/*", -- Ignore Laravel storage directory
+	"*/vendor/*", -- Ignore Laravel vendor directory (for Composer dependencies)
+	"*/bootstrap/cache/*", -- Ignore Laravel bootstrap cache directory
+	"*/public/hot/*", -- Ignore Laravel Mix hot reload file
+	"*/.phpunit.result.cache", -- Ignore PHPUnit cache file
+})
 
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
