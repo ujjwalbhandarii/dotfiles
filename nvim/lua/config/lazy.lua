71f0fcd6458983f962880d9547513cdea3f20d3c
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-03-12 09:57:06 +0545

    refactoring lazy.lua

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,19 +23,15 @@ require("lazy").setup({
 			import = "lazyvim.plugins",
 			opts = {
 				colorscheme = "tokyonight-night",
-				news = {
-					lazyvim = true,
-					neovim = true,
-				},
 			},
 		},
-		{ import = "lazyvim.plugins.extras.linting.eslint" },
-		{ import = "lazyvim.plugins.extras.formatting.prettier" },
-		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "plugins" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
+		{ import = "lazyvim.plugins.extras.linting.eslint" },
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-		{ import = "plugins" },
 	},
 	defaults = {
 		lazy = false,
