21ccfea9e1078b5d984c36997fbbd612dc8a7aa3
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-03-12 17:18:05 +0545

    removed comment keymaps

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,10 +1,4 @@
 return {
 	"numToStr/Comment.nvim",
-	-- opts = {
-	-- 	toggler = {
-	-- 		line = "<C-/>", -- Use Ctrl + / for line comments
-	-- 		block = "<C-?>", -- Use Ctrl + ? for block comments
-	-- 	},
-	-- },
 	lazy = false,
 }
