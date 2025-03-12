b69220720e009705646db67290235577d56277d5
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-03-12 17:16:11 +0545

    updated keymaps mess

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,7 +5,6 @@ local opts = { noremap = true, silent = true }
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Split window
--- TODO - Fix Horizontal Split
 keymap.set("n", "<leader>h", ":split<Return>", opts) -- Horizontal
 keymap.set("n", "<leader>v", ":vsplit<Return>", opts) -- Vertical
 
@@ -14,3 +13,6 @@ keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
 keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
+-- Keymaps for switching tabs
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
